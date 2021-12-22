0a2649415c043e29d3f156fe55311beb06ca32a0
Ge0h <george.walker.wme@gmail.com>
2021-12-22 17:28:05 +0000

    Updates

--- js/contact.js
+++ js/contact.js
@@ -9,17 +9,17 @@ const contact = () => {
     const facebook = document.createElement('li')
     facebook.textContent = 'Facebook'
     const fbIcon = document.createElement('i')
-    fbIcon.classList.add('fab', 'fa-facebook')
+    fbIcon.classList.add('fab', 'fa-facebook', 'py-1')
 
     const twitter = document.createElement('li')
     twitter.textContent = 'Twitter'
     const twitterIcon = document.createElement('i')
-    twitterIcon.classList.add('fab', 'fa-twitter')
+    twitterIcon.classList.add('fab', 'fa-twitter', 'py-1')
 
     const insta = document.createElement('li')
     insta.textContent = 'Instagram'
     const instaIcon = document.createElement('i')
-    instaIcon.classList.add('fab', 'fa-instagram')
+    instaIcon.classList.add('fab', 'fa-instagram', 'py-1')
 
     const deliveroo = document.createElement('li')
     deliveroo.textContent = 'Deliveroo'
