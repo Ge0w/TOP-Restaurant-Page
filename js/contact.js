e39a2f0af5d121ce7eccd6440bfc188dba5fb71b
Ge0h <george.walker.wme@gmail.com>
2021-12-22 15:05:43 +0000

    Updates

--- js/contact.js
+++ js/contact.js
@@ -2,9 +2,22 @@ const contact = () => {
     const contact = document.createElement('section')
     contact.classList.add('contact','order','flex')
 
-    const details = document.createElement('h1')
-    details.textContent = 'Contact details here'
-    contact.append(details)
+    const facebook = document.createElement('h1')
+    facebook.textContent = 'Facebook'
+
+    const twitter = document.createElement('h1')
+    twitter.textContent = 'Twitter'
+
+    const insta = document.createElement('h1')
+    insta.textContent = 'Facebook'
+
+    const deliveroo = document.createElement('h1')
+    deliveroo.textContent = 'Deliveroo'
+
+    contact.append(facebook)
+    contact.append(twitter)
+    contact.append(insta)
+    contact.append(deliveroo)
 
     return contact
 }
