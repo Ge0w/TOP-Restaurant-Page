c1cac5afefc84a2021303ee53a3b8d2fc96321f9
Ge0h <george.walker.wme@gmail.com>
2022-04-18 14:16:35 +0100

    Refactor: Menu page made fully dynamic

--- dist/js/menu.js
+++ dist/js/menu.js
@@ -26,16 +26,19 @@ const menu = () => {
       title: "Mixed Brownie Boxes",
       price: "£15.50",
       description: "Box of mixed brownies",
+      img: "images/brownies.jpg",
     },
     {
       title: "Gooey Cookies",
       price: "£12.50",
       description: "Box of our best yet mixed gooey cookies",
+      img: "images/cookies.jpg",
     },
     {
       title: "Colourful Cupcakes",
       price: "£14.95",
       description: "Box of colourful cupboards, spongey and tasty",
+      img: "images/cupcakes.jpg",
     },
   ];
 
@@ -43,77 +46,20 @@ const menu = () => {
   for (let i = 0; i < menuItems.length; i++) {
     const menuItem = document.createElement("div");
     menuContainer.append(menuItem);
-    menuItem.textContent = `${menuItems[i].title}`;
+    menuItem.classList.add("flex", "menu-item");
+    const menuImg = document.createElement("img");
+    menuImg.classList.add("menu-img");
+    const menuText = document.createElement("div");
+    const title = document.createElement("h4");
+    const description = document.createElement("p");
+    const span = document.createElement("span");
+    menuItem.append(menuImg, menuText);
+    menuText.append(title, span, description);
+    span.textContent = `${menuItems[i].price}`;
+    title.textContent = `${menuItems[i].title}`;
+    description.textContent = `${menuItems[i].description}`;
+    menuImg.src = `${menuItems[i].img}`;
   }
-
-  //   menu.innerHTML = `<section class='menu flex my-1 py-1'>
-  //         <h1>Milkshakes</h1>
-  //         <div class='milkshakes flex'>
-  //             <div class='card flex'>
-  //                 <img src='images/icon_logo.png '>
-  //                 <h2>Vanilla Milkshake</h2>
-  //                 <h3>£4.99</h3>
-  //             </div>
-  //             <div class='card flex'>
-  //                 <img src='images/icon_logo.png '>
-  //                 <h2>Vanilla Milkshake</h2>
-  //                 <h3>£4.99</h3>
-  //             </div>
-  //             <div class='card flex'>
-  //                 <img src='images/icon_logo.png '>
-  //                 <h2>Vanilla Milkshake</h2>
-  //                 <h3>£4.99</h3>
-  //             </div>
-  //             <div class='card flex'>
-  //                 <img src='images/icon_logo.png '>
-  //                 <h2>Vanilla Milkshake</h2>
-  //                 <h3>£4.99</h3>
-  //             </div>
-  //             <div class='card flex'>
-  //                 <img src='images/icon_logo.png '>
-  //                 <h2>Vanilla Milkshake</h2>
-  //                 <h3>£4.99</h3>
-  //             </div>
-  //             <div class='card flex'>
-  //                 <img src='images/icon_logo.png '>
-  //                 <h2>Vanilla Milkshake</h2>
-  //                 <h3>£4.99</h3>
-  //             </div>
-  //             <div class='card flex'>
-  //                 <img src='images/icon_logo.png '>
-  //                 <h2>Vanilla Milkshake</h2>
-  //                 <h3>£4.99</h3>
-  //             </div>
-  //             <div class='card flex'>
-  //                 <img src='images/icon_logo.png '>
-  //                 <h2>Vanilla Milkshake</h2>
-  //                 <h3>£4.99</h3>
-  //             </div>
-  //         </div>
-  //         <h1>Baked Goods</h1>
-  //         <div class='baked-goods flex'>
-  //             <div class='card flex'>
-  //                 <img src='images/icon_logo.png '>
-  //                 <h2>Vanilla Milkshake</h2>
-  //                 <h3>£4.99</h3>
-  //             </div>
-  //             <div class='card flex'>
-  //                 <img src='images/icon_logo.png '>
-  //                 <h2>Vanilla Milkshake</h2>
-  //                 <h3>£4.99</h3>
-  //             </div>
-  //             <div class='card flex'>
-  //                 <img src='images/icon_logo.png '>
-  //                 <h2>Vanilla Milkshake</h2>
-  //                 <h3>£4.99</h3>
-  //             </div>
-  //             <div class='card flex'>
-  //                 <img src='images/icon_logo.png '>
-  //                 <h2>Vanilla Milkshake</h2>
-  //                 <h3>£4.99</h3>
-  //             </div>
-  //         </div>
-  //         </section>`;
   return menu;
 };
 
