47e6545d8ead3be929dccd6dfdcdba371d41e6d2
Ge0h <george.walker.wme@gmail.com>
2022-04-18 21:51:27 +0100

    Refactor: Mobile friendly navigation bar added

--- dist/js/header.js
+++ dist/js/header.js
@@ -2,13 +2,13 @@ const header = () => {
   const header = document.createElement("div");
   header.classList.add("header", "flex");
 
-  const flexContainer = document.createElement("div");
-  flexContainer.classList.add("flex");
-  header.append(flexContainer);
+  const firstRow = document.createElement("div");
+  firstRow.classList.add("flex");
+  header.append(firstRow);
 
   const logoContainer = document.createElement("div");
   logoContainer.classList.add("flex", "logo");
-  flexContainer.append(logoContainer);
+  firstRow.append(logoContainer);
 
   const headerText = document.createElement("h1");
   headerText.classList.add("my-1-2", "py-1-2");
@@ -21,13 +21,9 @@ const header = () => {
   img.alt = "Logo";
   logoContainer.append(img);
 
-  const navbarContainer = document.createElement("div");
-  navbarContainer.classList.add("navbar", "flex");
-  flexContainer.append(navbarContainer);
-
   const navbar = document.createElement("navbar");
-  navbar.classList.add("flex");
-  navbarContainer.append(navbar);
+  navbar.classList.add("flex", "navbar");
+  header.append(navbar);
 
   const uList = document.createElement("ul");
   uList.classList.add("flex");
@@ -47,6 +43,35 @@ const header = () => {
   contact.id = "contactBtn";
   contact.textContent = "Contact";
   uList.append(contact);
+
+  // Burger button
+  const burgerContainer = document.createElement("div");
+  burgerContainer.classList.add("burger-container");
+  firstRow.append(burgerContainer);
+  const burgerButton = document.createElement("a");
+  const xButton = document.createElement("a");
+  const mobileIcon = document.createElement("i");
+  const xIcon = document.createElement("i");
+  xIcon.classList.add("fas", "fa-times", "fa-2x", "mobile-icon");
+  mobileIcon.classList.add("fas", "fa-bars", "fa-2x", "mobile-icon");
+  burgerContainer.append(burgerButton, xButton);
+  burgerButton.append(mobileIcon);
+  xButton.append(xIcon);
+  xButton.style.display = "none";
+
+  burgerButton.addEventListener("click", () => {
+    header.style.flexDirection = "column";
+    navbar.style.display = "flex";
+    burgerButton.style.display = "none";
+    xButton.style.display = "inline-block";
+  });
+
+  xButton.addEventListener("click", () => {
+    header.style.flexDirection = "row";
+    navbar.style.display = "none";
+    xButton.style.display = "none";
+    burgerButton.style.display = "inline-block";
+  });
 
   return header;
 };
