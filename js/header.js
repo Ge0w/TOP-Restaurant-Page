5e4fc8e4d6c47e4302e7b97043de3f775899fd51
Ge0h <george.walker.wme@gmail.com>
2022-04-18 21:48:15 +0100

    Updates

--- js/header.js
+++ js/header.js
@@ -2,9 +2,13 @@ const header = () => {
   const header = document.createElement("div");
   header.classList.add("header", "flex");
 
+  const firstRow = document.createElement("div");
+  firstRow.classList.add("flex");
+  header.append(firstRow);
+
   const logoContainer = document.createElement("div");
   logoContainer.classList.add("flex", "logo");
-  header.append(logoContainer);
+  firstRow.append(logoContainer);
 
   const headerText = document.createElement("h1");
   headerText.classList.add("my-1-2", "py-1-2");
@@ -40,10 +44,34 @@ const header = () => {
   contact.textContent = "Contact";
   uList.append(contact);
 
+  // Burger button
+  const burgerContainer = document.createElement("div");
+  burgerContainer.classList.add("burger-container");
+  firstRow.append(burgerContainer);
+  const burgerButton = document.createElement("a");
+  const xButton = document.createElement("a");
   const mobileIcon = document.createElement("i");
+  const xIcon = document.createElement("i");
+  xIcon.classList.add("fas", "fa-times", "fa-2x", "mobile-icon");
   mobileIcon.classList.add("fas", "fa-bars", "fa-2x", "mobile-icon");
-  mobileIcon.id = "mobileIcon";
-  navbar.append(mobileIcon);
+  burgerContainer.append(burgerButton, xButton);
+  burgerButton.append(mobileIcon);
+  xButton.append(xIcon);
+  xButton.style.display = "none";
+
+  burgerButton.addEventListener("click", () => {
+    header.style.flexDirection = "column";
+    navbar.style.display = "flex";
+    burgerButton.style.display = "none";
+    xButton.style.display = "inline-block";
+  });
+
+  xButton.addEventListener("click", () => {
+    header.style.flexDirection = "row";
+    navbar.style.display = "none";
+    xButton.style.display = "none";
+    burgerButton.style.display = "inline-block";
+  });
 
   return header;
 };
