4f4fef456bf9f780468a4fb06ae1dece2fbfe12b
Ge0h <george.walker.wme@gmail.com>
2022-04-19 20:58:29 +0100

    Feature: Navigation bar mobile friendly complete

--- dist/js/header.js
+++ dist/js/header.js
@@ -1,9 +1,9 @@
 const header = () => {
-  const header = document.createElement("div");
+  const header = document.createElement("section");
   header.classList.add("header", "flex");
 
   const firstRow = document.createElement("div");
-  firstRow.classList.add("flex");
+  firstRow.classList.add("flex", "logo");
   header.append(firstRow);
 
   const logoContainer = document.createElement("div");
@@ -21,13 +21,28 @@ const header = () => {
   img.alt = "Logo";
   logoContainer.append(img);
 
-  const navbar = document.createElement("navbar");
-  navbar.classList.add("flex", "navbar");
-  header.append(navbar);
+  // Burger button
+  const burgerContainer = document.createElement("div");
+  burgerContainer.classList.add("burger-container");
+  firstRow.append(burgerContainer);
+  const burgerButton = document.createElement("a");
+  const xButton = document.createElement("a");
+  const mobileIcon = document.createElement("i");
+  const xIcon = document.createElement("i");
+  xIcon.classList.add("fas", "fa-times", "fa-2x", "mobile-icon");
+  mobileIcon.classList.add("fas", "fa-bars", "fa-2x", "mobile-icon");
+  burgerContainer.append(burgerButton, xButton);
+  burgerButton.append(mobileIcon);
+  xButton.append(xIcon);
+  xButton.style.display = "none";
+
+  const links = document.createElement("navbar");
+  links.classList.add("flex", "navbar", "links");
+  header.append(links);
 
   const uList = document.createElement("ul");
   uList.classList.add("flex");
-  navbar.append(uList);
+  links.append(uList);
 
   const about = document.createElement("li");
   about.id = "aboutBtn";
@@ -44,31 +59,27 @@ const header = () => {
   contact.textContent = "Contact";
   uList.append(contact);
 
-  // Burger button
-  const burgerContainer = document.createElement("div");
-  burgerContainer.classList.add("burger-container");
-  firstRow.append(burgerContainer);
-  const burgerButton = document.createElement("a");
-  const xButton = document.createElement("a");
-  const mobileIcon = document.createElement("i");
-  const xIcon = document.createElement("i");
-  xIcon.classList.add("fas", "fa-times", "fa-2x", "mobile-icon");
-  mobileIcon.classList.add("fas", "fa-bars", "fa-2x", "mobile-icon");
-  burgerContainer.append(burgerButton, xButton);
-  burgerButton.append(mobileIcon);
-  xButton.append(xIcon);
-  xButton.style.display = "none";
+  // burgerButton.addEventListener("click", () => {
+  //   header.style.flexDirection = "column";
+  //   navbar.style.display = "flex";
+  //   burgerButton.style.display = "none";
+  //   xButton.style.display = "inline-block";
+  // });
 
+  // xButton.addEventListener("click", () => {
+  //   header.style.flexDirection = "row";
+  //   navbar.style.display = "none";
+  //   xButton.style.display = "none";
+  //   burgerButton.style.display = "inline-block";
+  // });
   burgerButton.addEventListener("click", () => {
-    header.style.flexDirection = "column";
-    navbar.style.display = "flex";
+    links.classList.toggle("active");
     burgerButton.style.display = "none";
     xButton.style.display = "inline-block";
   });
 
   xButton.addEventListener("click", () => {
-    header.style.flexDirection = "row";
-    navbar.style.display = "none";
+    links.classList.toggle("active");
     xButton.style.display = "none";
     burgerButton.style.display = "inline-block";
   });
